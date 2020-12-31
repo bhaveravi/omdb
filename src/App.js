a11b37d8d9e1302a88df586f7aa83ff43cf174aa
Ravi Bhave <RaviB@WinjitTechnologiesPvtLtd.local>
2020-12-31 23:23:08 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Movie from './Components/movie-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      show:false,
+      movieName:''
+    };
+  }
+  
+ SearchMovie= (e) =>{
+    this.setState(state=>({show:true}));
+    //this.setState(state=>({movieName:e.target.value}));
+    //this.setState(state=>({movieName:e.target.value,show:true}));
+    console.log(this.state.movieName);
+  }
+
+  textChange = (e) =>{
+    this.setState(state=>({movieName:e.target.value,show:false}));
+  }
+  render(){
+    return (
+      <div className="App">
+        
+          <h1>
+           IMDB
+          </h1>
+          <input type="search" id="txtSearch" className="App-button" onChange={this.textChange} placeholder="Search movie by name"></input>
+          <br/>
+          <br/>
+          <button className="App-button" onClick={this.SearchMovie} >Search</button>
+        {this.state.show?<Movie movieName={this.state.movieName}/>:null}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/movie-component.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import '../App.css';
+//http://www.omdbapi.com/?i=tt3896198&apikey=2128c072&t=movieName
+/*
+export const Movie = (props) => (
+    <div>
+    <img src={props.Poster} alt={props.Title} />
+    <h1>{props.Title}</h1>
+    <h4>Actor : {props.Actors}</h4>
+    <h4>Production : {props.Production}</h4>
+    <h4>Director : {props.Director}</h4>
+    <h4>IMDB Rating : {props.imdbRating}</h4>
+    <p>
+    {props.Plot}
+    </p>
+</div>
+);
+*/
+
+class Movie extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            movieData: ''
+          }
+
+    }
+    async componentDidMount(){
+        /*fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2128c072&t='+this.props.movieName)
+        .then(response => console.log(response.json()))
+        .then(result => {
+            this.setState({movieData: console.log(result)})
+        })
+
+        console.log(this.state.movieData);*/
+        
+       
+       const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=2128c072&plot=full&t='+this.props.movieName;
+       const response = await fetch(url);
+       const data = await response.json();
+       this.setState({movieData:data});
+       console.log(this.state.movieData);
+       
+      /*fetch(url)
+      .then(response=>response.json())
+       .then(data=>{
+         this.setState({
+             movieData : data
+         })
+
+       });
+       console.log(this.state.movieData);*/
+     
+      }
+    
+    render(){
+        const {movieData} = this.state;
+        return(
+            <div>
+                <img src={movieData.Poster} alt={movieData.Title} />
+                <h1>{movieData.Title}</h1>
+                <h4>Actor : {movieData.Actors}</h4>
+                <h4>Production : {movieData.Production}</h4>
+                <h4>Director : {movieData.Director}</h4>
+                <h4>IMDB Rating : {movieData.imdbRating}</h4>
+                <p>
+                {movieData.Plot}
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Movie;
